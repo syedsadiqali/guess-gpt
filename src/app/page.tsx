7746ae3f0c43a80d4a4a72449106a52bde413bcb
Syed Ali <syedsadiqali2018@gmail.com>
2024-05-26 17:53:04 +0530

    added metadata base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next"
 import Chat from "./client"
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://guess-gpt.vercel.app/'),
   title: 'Guess GPT : Guess what\'s in the image',
   description: 'Guess GPT is a AI Vision App that helps you identify Places / People / Things in an Image.',
 }
